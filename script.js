cbae28a57d1af08f71b3adbef09126f7d57c50d0
Johnny <43801242+JohnnyASan@users.noreply.github.com>
2019-03-09 00:03:57 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let questions = new Vue({
+  el: '#questions',
+  data: {
+    addedName: '',
+    addedQuestion: '',
+    questions:{
+      upVotes:'',
+      downVotes:'',
+      questionAsked:'',
+    },
+
+  },
+  created() {
+
+  },
+  methods: {
+
+  }
+
+
+});
